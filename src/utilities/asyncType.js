51f39de43866661145980f75d6137e96b9929a19
Jiří Čermák <jiri.cermak@ackee.cz>
2019-03-20 13:44:48 +0100

    Update asyncType util. - add RESET action type

--- src/utilities/asyncType.js
+++ src/utilities/asyncType.js
@@ -13,7 +13,7 @@
     });
  */
 
-const DEFAULT_TYPES = ['REQUEST', 'SUCCESS', 'FAILURE', 'INVALIDATE'];
+const DEFAULT_TYPES = ['REQUEST', 'SUCCESS', 'FAILURE', 'INVALIDATE', 'RESET'];
 
 export function asyncType(params = {}) {
     const { types, typePrefix, modulePrefix } = {
