74b6c5b05d769a02bb20379bdf47251da1ff357f
Jiří Čermák <jiri.cermak@ackee.cz>
2019-04-28 00:05:55 +0200

    Add missing export

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,3 +1,3 @@
 export { default as createType } from './createType';
-export { asyncType, moduleAsyncType } from './asyncType';
+export { asyncType, moduleAsyncType, createAsyncType } from './asyncType';
 export { default as strictObjectAccess } from './strictObjectAccess';
