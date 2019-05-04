c7888e06d1ea9102a2aac48ff417ab1916968074
Jiří Čermák <jiri.cermak@ackee.cz>
2019-05-04 16:14:13 +0200

    Add aliases to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { jestAliases } = require('./config/aliases');
+
 module.exports = {
-    testPathIgnorePatterns: ['/node_modules', '/lib', '/es'],
+    testPathIgnorePatterns: ['node_modules', 'lib', 'es'],
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js?$',
     moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+    moduleNameMapper: jestAliases,
 };
