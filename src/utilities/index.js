fa11f13c3f85966e2f614c96db5fac1f930874bf
Jiří Čermák <jiri.cermak@ackee.cz>
2019-05-04 13:05:05 +0200

    Remove createType in favor of asyncType utility

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,3 +1,2 @@
-export { default as createType } from './createType';
-export { asyncType, moduleAsyncType, createAsyncType } from './asyncType';
+export { asyncType, createAsyncType } from './asyncType';
 export { default as strictObjectAccess } from './strictObjectAccess';
