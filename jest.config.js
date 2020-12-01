e67a7f7151f963948d521755d807abc265739f45
Jakub Baierl <jakub.baierl@gmail.com>
2020-12-01 16:55:28 +0100

    :wrench: jest config changed
    jest config changed

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,8 @@
 const { jestAliases } = require('./config/aliases');
 
 module.exports = {
-    roots: ['<rootDir>/src'],
-    testPathIgnorePatterns: ['/node_modules', '/lib', '/es'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/'],
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js?$',
+    moduleFileExtensions: ['ts', 'js', 'jsx', 'json', 'node'],
     moduleNameMapper: jestAliases,
-    globals: {
-        'ts-jest': {
-            diagnostics: false,
-        },
-    },
 };
