c113cac77d82360a85e3cd9493cecfe4d9077061
Jakub Baierl <jakub.baierl@gmail.com>
2021-03-03 14:10:16 +0100

    :label: generic types added
    generic types added

--- src/config/reducers/containerReducer.ts
+++ src/config/reducers/containerReducer.ts
@@ -4,12 +4,12 @@ import { Action, ApiReducerState } from './types';
 export default function configureContainerReducer(customConfigure?: any) {
     const initialState: ApiReducerState = Object.freeze({});
 
-    const options: any = Object.freeze({
+    const options: object = Object.freeze({
         ignoreWarnings: process.env.NODE_ENV !== 'development',
         placeholder: true,
     });
 
-    const selectors: any = Object.freeze({
+    const selectors: object = Object.freeze({
         itemId: (action: Action) => action.meta.itemId,
     });
 
