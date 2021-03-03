f12a26c33ebf214090b53271bfc1cf859be90f66
Jakub Baierl <jakub.baierl@gmail.com>
2021-03-03 12:51:12 +0100

    :wrench: configs updated, package.json updated
    configs updated, package.json updated

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const { webpackAliases } = require('./config/aliases');
-
-module.exports = {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    resolve: {
-        alias: webpackAliases,
-    },
-};
