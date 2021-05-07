6b3d56e80fbc8a59a714540928602b50f74e4db8
Jiří Zdvomka <jiri.zdvomka@ackee.cz>
2021-05-07 22:40:50 +0200

    🏷️  Update types for apiRequestActions

--- src/actions/apiRequest/apiRequestActions.ts
+++ src/actions/apiRequest/apiRequestActions.ts
@@ -9,8 +9,11 @@ const defaultOptions: Options = {
     isDetailRequest: false,
 };
 
-const apiRequestActions = (types: Record<string, string>, options: Options = defaultOptions): object => {
-    const actionCreators = Object.values(types).reduce((actions: object, type: string) => {
+const apiRequestActions = (
+    types: Record<string, string>,
+    options: Options = defaultOptions,
+): Record<string, Function> => {
+    const actionCreators = Object.values(types).reduce((actions, type) => {
         const template = options.isDetailRequest ? detail : list;
         const name = convertTypeToActionName(type);
         const suffix = type.slice(type.lastIndexOf('_') + 1);
