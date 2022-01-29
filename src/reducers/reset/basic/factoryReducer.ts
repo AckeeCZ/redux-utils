1eab08ab5578f16988fecb751d88418b282fcfc1
Martin Foldyna <martin.foldyna@ackee.cz>
2022-01-29 21:15:28 +0100

    :label: Properly type basicResetReducer

--- src/reducers/reset/basic/factoryReducer.ts
+++ src/reducers/reset/basic/factoryReducer.ts
@@ -1,24 +1,26 @@
-import { ApiReducerState, Action } from '../../../config';
 import { parseResetActionType } from '../utilities';
+import { AnyAction } from '@reduxjs/toolkit';
+type Reducer = (state: any, action: AnyAction) => any;
 
 /**
  * Reset reducer to initial state when 'actionType' is dispatched.
  *
  * Docs: https://github.com/AckeeCZ/redux-utils/blob/master/docs/reducers/basicResetReducer.md
  *
- * @param {(state: object, action: object) => object} reducer
- * @param {string | string[]} actionType
- * @returns {(state: object, action: object) => object}
  */
-export default function makeResetReducer(reducer: Function, actionType: string) {
+export default function makeResetReducer<R extends Reducer>(reducer: R, actionType: string | string[]) {
     const actionTypes = new Set(parseResetActionType(actionType));
 
-    const resetReducer = (state: ApiReducerState, action: Action) => {
+    type State = Parameters<R>[0];
+    type Action = Parameters<R>[1];
+
+    const resetReducer = (state: State, action: Action): ReturnType<R> => {
         // if actionType matches to the current action.type,
         // then get initialState by passing undefined state to the reducer
         const newState = actionTypes.has(action.type) ? undefined : state;
 
         return reducer(newState, action);
     };
+
     return resetReducer;
 }
