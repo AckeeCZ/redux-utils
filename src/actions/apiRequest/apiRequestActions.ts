34cae8a72bff51bc8206ef8180fd10df3379fd0a
Lukas Horak <horaklukas@centrum.cz>
2022-02-25 15:26:40 +0100

    🏷️ First tier, map action names in apiRequestActions

--- src/actions/apiRequest/apiRequestActions.ts
+++ src/actions/apiRequest/apiRequestActions.ts
@@ -1,4 +1,4 @@
-import { convertTypeToActionName } from './utils';
+import { convertTypeToActionName, TypeToActionName } from './utils';
 import { list, detail } from './templates';
 
 interface Options {
@@ -9,20 +9,21 @@ const defaultOptions: Options = {
     isDetailRequest: false,
 };
 
-const apiRequestActions = (
-    types: Record<string, string>,
-    options: Options = defaultOptions,
-): Record<string, Function> => {
+const apiRequestActions = <T extends { [KT: string]: string }>(types: T, options: Options = defaultOptions) => {
+    type ActionCreators = {
+        [K in keyof T as TypeToActionName<K>]: Function;
+    };
+
     const actionCreators = Object.values(types).reduce((actions, type) => {
         const template = options.isDetailRequest ? detail : list;
-        const name = convertTypeToActionName(type);
+        const name = convertTypeToActionName(type) as keyof ActionCreators;
         const suffix = type.slice(type.lastIndexOf('_') + 1);
 
         actions[name] = template[suffix](type);
         return actions;
-    }, {});
+    }, {} as ActionCreators);
 
-    return Object.freeze(actionCreators);
+    return Object.freeze<ActionCreators>(actionCreators);
 };
 
 export default apiRequestActions;
--- src/actions/apiRequest/utils.ts
+++ src/actions/apiRequest/utils.ts
@@ -5,3 +5,9 @@ export const convertTypeToActionName = (type: string) => {
     const parts: string[] = unprefixed.split('_');
     return parts.slice(1).reduce((name, part) => name + capitalizeFirstLetter(part), parts[0]);
 };
+
+type SnakeCaseToCamelCase<S> = S extends `${infer H}_${infer R}` ? SnakeCaseToCamelCase<`${H}${Capitalize<R>}`> : S;
+
+export type TypeToActionName<T extends string | number | symbol> = T extends string
+    ? SnakeCaseToCamelCase<Lowercase<T extends `${any}/${infer Unprefixed}` ? Unprefixed : T>>
+    : never;
